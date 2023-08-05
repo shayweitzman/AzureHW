48c51d580035410e5f789c189bda26807b34aeac
Shay Weitzman <shaywe@ac.sce.ac.il>
2023-08-06 02:54:47 +0300

    Add Windows template and deploy script.

---
+++ windowsDeploy.ps1
@@ -0,0 +1,7 @@
+$templateFile = "windowsTemplate.json"
+New-AzResourceGroupDeployment `
+    -ResourceGroupName "WindowsRG" `
+    -TemplateFile $templateFile `
+    -adminUsername "shaywinuser" `
+    -adminPassword "shayWinPass!123" `
+    -dnsLabelPrefix "shaywindns"
