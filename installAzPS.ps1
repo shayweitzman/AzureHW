a7f4e5b86480d8a2dc6c4ac07cd1808985eefce5
Shay Weitzman <shaywe@ac.sce.ac.il>
2023-08-09 02:24:35 +0300

    Update installAzPS.ps1

--- installAzPS.ps1
+++ installAzPS.ps1
@@ -1,7 +1,18 @@
 $vmName = 'simple-vm'
 $resourceGroupName = 'WindowsRG'
+# Write-Host "Installing Azure-CLI to $vmName"
+# Invoke-AzVMRunCommand `
+# -ResourceGroupName $resourceGroupName `
+# -VMName $vmName `
+# -CommandId 'RunPowerShellScript' `
+# -ScriptString '$ProgressPreference = ''SilentlyContinue''; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList ''/I AzureCLI.msi /quiet''; Remove-Item .\AzureCLI.msi'
+# Write-Host "Azure-CLI Installed successfully to $vmName!"
+Write-Host "Connecting Azure..."
+az vm identity assign -g $resourceGroupName -n $vmName  --identities shayIdentity
 Invoke-AzVMRunCommand `
 -ResourceGroupName $resourceGroupName `
 -VMName $vmName `
 -CommandId 'RunPowerShellScript' `
--ScriptString 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;Install-Module -Name Az -Scope AllUsers -Force'
+-ScriptString 'az login --identity'
+
+Write-Host "Connected!"
